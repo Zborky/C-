69a4b6ea35f6f5e76469097d01ce915e3198a4a0
Zborky <jakubzboron11@gmail.com>
2024-09-08 23:06:14 +0200

    SkolskaUlohovaApka.cpp

--- SkolskaAppkaNaUlohy/SkolskaUlohovaApka.cpp
+++ SkolskaAppkaNaUlohy/SkolskaUlohovaApka.cpp
@@ -1,314 +1,249 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <fstream>
-#include <algorithm>
 
 using namespace std;
 
-// Trieda HomeWork
-class HomeWork {
+/**
+ * The Course class represents a course.
+ */
+class Course {
 private:
-    int homeworkID;
-    string title;
-    string description;
-    string dueDate;
-    int courseID;
+    int kredits; // Number of credits for the course
+    string profesor; // Name of the professor teaching the course
+    string course_name; // Name of the course
+    string course_end; // End time of the course
+    string course_start; // Start time of the course
+    vector<string> meeting_days; // Days the course is held
 
 public:
     /**
-     * Konštruktor pre triedu HomeWork.
-     *
-     * @param homeworkID ID domácej úlohy.
-     * @param title Názov domácej úlohy.
-     * @param description Popis domácej úlohy.
-     * @param dueDate Dátum odovzdania domácej úlohy.
-     * @param courseID ID kurzu.
+     * Constructor for the Course class.
+     * @param kredits Number of credits for the course.
+     * @param profesor Name of the professor.
+     * @param course_name Name of the course.
+     * @param course_end End time of the course.
+     * @param course_start Start time of the course.
+     * @param meeting_days Days the course is held.
      */
-    HomeWork(int homeworkID, string title, string description, string dueDate, int courseID)
-        : homeworkID(homeworkID), title(title), description(description), dueDate(dueDate), courseID(courseID) {}
-
-    /**
-     * Zobrazí informácie o domácej úlohe.
-     */
-    void displayHomeWork() const {
-        cout << "HomeWorkID: " << homeworkID << "\nTitle: " << title << "\nDescription: " << description
-            << "\nDue Date: " << dueDate << "\nCourseID: " << courseID << endl;
+    Course(int kredits, string profesor, string course_name, string course_end, string course_start, vector<string> meeting_days) {
+        this->kredits = kredits;
+        this->profesor = profesor;
+        this->course_name = course_name;
+        this->course_end = course_end;
+        this->course_start = course_start;
+        this->meeting_days = meeting_days;
     }
 
     /**
-     * Uloží domácu úlohu do súboru.
-     *
-     * @param outFile Výstupný súborový prúd.
+     * Gets the number of credits for the course.
+     * @return The number of credits.
      */
-    void saveToFile(ofstream& outFile) const {
-        outFile << homeworkID << "\n" << title << "\n" << description << "\n" << dueDate << "\n" << courseID << "\n";
+    int getKredits() const {
+        return this->kredits;
     }
 
     /**
-     * Naèíta domácu úlohu zo súboru.
-     *
-     * @param inFile Vstupný súborový prúd.
-     * @return Naèítaná domáca úloha.
+     * Gets the name of the professor.
+     * @return The name of the professor.
      */
-    static HomeWork loadFromFile(ifstream& inFile) {
-        int id, cID;
-        string t, d, date;
-        inFile >> id;
-        inFile.ignore();
-        getline(inFile, t);
-        getline(inFile, d);
-        getline(inFile, date);
-        inFile >> cID;
-        inFile.ignore();
-        return HomeWork(id, t, d, date, cID);
+    string getProfesor() const {
+        return this->profesor;
     }
 
-    int getHomeworkID() const { return homeworkID; }
-    const string& getTitle() const { return title; }
-    const string& getDescription() const { return description; }
-    const string& getDueDate() const { return dueDate; }
-    int getCourseID() const { return courseID; }
+    /**
+     * Gets the name of the course.
+     * @return The name of the course.
+     */
+    string getCourse_Name() const {
+        return this->course_name;
+    }
+
+    /**
+     * Gets the start time of the course.
+     * @return The start time of the course.
+     */
+    string getCourse_Start() const {
+        return this->course_start;
+    }
+
+    /**
+     * Gets the end time of the course.
+     * @return The end time of the course.
+     */
+    string getCourse_End() const {
+        return this->course_end;
+    }
+
+    /**
+     * Gets the days the course is held.
+     * @return A vector of meeting days.
+     */
+    const vector<string>& getMeeting_Days() const {
+        return meeting_days;
+    }
+
+    /**
+     * Sets the number of credits for the course.
+     * @param kredits The number of credits.
+     */
+    void setKredits(int kredits) {
+        this->kredits = kredits;
+    }
+
+    /**
+     * Sets the name of the professor.
+     * @param profesor The name of the professor.
+     */
+    void setProfesor(string profesor) {
+        this->profesor = profesor;
+    }
+
+    /**
+     * Sets the name of the course.
+     * @param name The name of the course.
+     */
+    void setCourse_Name(string name) {
+        this->course_name = name;
+    }
+
+    /**
+     * Sets the end time of the course.
+     * @param time The end time of the course.
+     */
+    void setCourse_End(string time) {
+        this->course_end = time;
+    }
+
+    /**
+     * Sets the start time of the course.
+     * @param time The start time of the course.
+     */
+    void setCourse_Start(string time) {
+        this->course_start = time;
+    }
 };
 
-// Trieda Student
-class Student {
+/**
+ * The Schedule class represents a schedule of courses.
+ */
+class Schedule {
 private:
-    int studentID;
-    string name;
-    string email;
+    vector<Course*> courses; // Vector of courses in the schedule
+    int number_of_courses; // Number of courses in the schedule
 
 public:
     /**
-     * Konštruktor pre triedu Student.
-     *
-     * @param id ID študenta.
-     * @param n Meno študenta.
-     * @param e E-mail študenta.
+     * Constructor for the Schedule class.
+     * @param number_of_courses Number of courses (initially 0).
      */
-    Student(int id, string n, string e)
-        : studentID(id), name(n), email(e) {}
-
-    /**
-     * Odovzdá domácu úlohu.
-     *
-     * @param homeworkID ID domácej úlohy.
-     * @param filePath Cesta k súboru.
-     */
-    void submitHomework(int homeworkID, string filePath) const {
-        cout << "Odovzdávam domácu úlohu s ID: " << homeworkID << " od " << name
-            << "\nCesta k súboru: " << filePath << endl;
+    Schedule(int number_of_courses) {
+        this->number_of_courses = 0;
     }
 
-    int getStudentID() const { return studentID; }
-    const string& getName() const { return name; }
-    const string& getEmail() const { return email; }
+    /**
+     * Adds a course to the schedule.
+     * @param course Pointer to the course to be added.
+     */
+    void addCourse(Course* course) {
+        courses.push_back(course); // Add course to the vector
+        this->number_of_courses++; // Increment course count
+    }
+
+    /**
+     * Removes a course from the schedule by course name.
+     * @param course_name The name of the course to remove.
+     */
+    void remove_Course(string course_name) {
+        for (int i = 0; i < number_of_courses; i++) {
+            if (courses[i]->getCourse_Name() == course_name) {
+                delete courses[i]; // Free dynamically allocated memory
+                courses.erase(courses.begin() + i); // Remove course from vector
+                this->number_of_courses--; // Decrement course count
+                break; // Exit loop after removing the course
+            }
+        }
+    }
+
+    /**
+     * Prints all the courses in the schedule.
+     */
+    void printSchedule() const {
+        for (const Course* course : courses) {
+            // Print course details
+            cout << "Course: " << course->getCourse_Name() << endl;
+            cout << "Days: ";
+            // Print meeting days
+            for (const string& day : course->getMeeting_Days()) {
+                cout << day << " ";
+            }
+            cout << endl;
+            // Print course times and professor
+            cout << "Time: " << course->getCourse_Start() << " - " << course->getCourse_End() << endl;
+            cout << "Professor: " << course->getProfesor() << endl << endl;
+        }
+    }
+
+    /**
+     * Checks for conflicts between two courses based on meeting days.
+     * @param course1 Pointer to the first course.
+     * @param course2 Pointer to the second course.
+     * @return True if there is a conflict (courses held on the same day), otherwise False.
+     */
+    bool checkConflikt(const Course* course1, const Course* course2) {
+        // Check if any meeting day overlaps
+        for (const string& day1 : course1->getMeeting_Days()) {
+            for (const string& day2 : course2->getMeeting_Days()) {
+                if (day1 == day2) {
+                    return true; // Conflict found
+                }
+            }
+        }
+        return false; // No conflict
+    }
 };
-
-// Trieda Submission
-class Submission {
-private:
-    int submissionID;
-    int homeworkID;
-    int studentID;
-    string filePath;
-    string submissionDate;
-    string grade;
-
-public:
-    /**
-     * Konštruktor pre triedu Submission.
-     *
-     * @param subID ID odovzdania.
-     * @param hwID ID domácej úlohy.
-     * @param studID ID študenta.
-     * @param path Cesta k súboru.
-     * @param date Dátum odovzdania.
-     * @param grd Známka.
-     */
-    Submission(int subID, int hwID, int studID, string path, string date, string grd)
-        : submissionID(subID), homeworkID(hwID), studentID(studID), filePath(path), submissionDate(date), grade(grd) {}
-
-    /**
-     * Uloží odovzdanie do súboru.
-     *
-     * @param outFile Výstupný súborový prúd.
-     */
-    void saveToFile(ofstream& outFile) const {
-        outFile << submissionID << "\n" << homeworkID << "\n" << studentID << "\n"
-            << filePath << "\n" << submissionDate << "\n" << grade << "\n";
-    }
-
-    /**
-     * Naèíta odovzdanie zo súboru.
-     *
-     * @param inFile Vstupný súborový prúd.
-     * @return Naèítané odovzdanie.
-     */
-    static Submission loadFromFile(ifstream& inFile) {
-        int subID, hwID, studID;
-        string path, date, grd;
-
-        inFile >> subID >> hwID >> studID;
-        inFile.ignore();
-        getline(inFile, path);
-        getline(inFile, date);
-        getline(inFile, grd);
-
-        return Submission(subID, hwID, studID, path, date, grd);
-    }
-
-    int getSubmissionID() const { return submissionID; }
-    int getHomeworkID() const { return homeworkID; }
-    int getStudentID() const { return studentID; }
-    const string& getFilePath() const { return filePath; }
-    const string& getSubmissionDate() const { return submissionDate; }
-    const string& getGrade() const { return grade; }
-};
-
-// Funkcie na uloženie a naèítanie domácich úloh
-/**
- * Uloží zoznam domácich úloh do súboru.
- *
- * @param homeworks Vektor domácich úloh.
- */
-void saveHomeworks(const vector<HomeWork>& homeworks) {
-    ofstream outFile("homeworks.txt");
-    for (const auto& hw : homeworks) {
-        hw.saveToFile(outFile);
-    }
-    outFile.close();
-}
-
-/**
- * Naèíta zoznam domácich úloh zo súboru.
- *
- * @return Vektor naèítaných domácich úloh.
- */
-vector<HomeWork> loadHomeworks() {
-    vector<HomeWork> homeworks;
-    ifstream inFile("homeworks.txt");
-    while (inFile.peek() != EOF) {
-        homeworks.push_back(HomeWork::loadFromFile(inFile));
-    }
-    inFile.close();
-    return homeworks;
-}
-
-// Funkcie na uloženie a naèítanie odovzdaných domácich úloh
-/**
- * Uloží zoznam odovzdaných domácich úloh do súboru.
- *
- * @param submissions Vektor odovzdaných domácich úloh.
- */
-void saveSubmissions(const vector<Submission>& submissions) {
-    ofstream outFile("submissions.txt");
-    for (const auto& sub : submissions) {
-        sub.saveToFile(outFile);
-    }
-    outFile.close();
-}
-
-/**
- * Naèíta zoznam odovzdaných domácich úloh zo súboru.
- *
- * @return Vektor naèítaných odovzdaných domácich úloh.
- */
-vector<Submission> loadSubmissions() {
-    vector<Submission> submissions;
-    ifstream inFile("submissions.txt");
-    while (inFile.peek() != EOF) {
-        submissions.push_back(Submission::loadFromFile(inFile));
-    }
-    inFile.close();
-    return submissions;
-}
 
 int main() {
-    vector<HomeWork> homeworks = loadHomeworks();
-    vector<Student> students;
-    vector<Submission> submissions = loadSubmissions();
+    // Create a schedule object
+    Schedule schedule(0);
 
-    students.push_back(Student(1, "John Doe", "john@example.com"));
+    // Create course objects dynamically
+    Course* course1 = new Course(3, "Prof. A", "Math", "10:00", "09:00", {"Mon", "Wed", "Fri"});
+    Course* course2 = new Course(4, "Prof. B", "Science", "11:00", "10:00", {"Tue", "Thu"});
+    Course* course3 = new Course(3, "Prof. C", "English", "09:00", "08:00", {"Mon", "Wed"});
 
-    int choice;
-    do {
-        cout << "Systém na správu domácich úloh\n";
-        cout << "1. Prida domácu úlohu\n";
-        cout << "2. Zobrazi domáce úlohy\n";
-        cout << "3. Odovzda domácu úlohu\n";
-        cout << "4. Ukonèi\n";
-        cout << "Zadajte svoju vo¾bu: ";
-        cin >> choice;
+    // Add courses to the schedule
+    schedule.addCourse(course1);
+    schedule.addCourse(course2);
+    schedule.addCourse(course3);
 
-        switch (choice) {
-        case 1: {
-            int id, courseID;
-            string title, description, dueDate;
-            cout << "Zadajte ID domácej úlohy: ";
-            cin >> id;
-            cout << "Zadajte názov: ";
-            cin.ignore();
-            getline(cin, title);
-            cout << "Zadajte popis: ";
-            getline(cin, description);
-            cout << "Zadajte dátum odovzdania: ";
-            getline(cin, dueDate);
-            cout << "Zadajte ID kurzu: ";
-            cin >> courseID;
-            homeworks.push_back(HomeWork(id, title, description, dueDate, courseID));
-            saveHomeworks(homeworks);
-            break;
-        }
-        case 2: {
-            for (const auto& hw : homeworks) {
-                hw.displayHomeWork();
-                cout << endl;
-            }
-            break;
-        }
-        case 3: {
-            int studentID, homeworkID;
-            string filePath;
-            cout << "Zadajte ID študenta: ";
-            cin >> studentID;
-            cout << "Zadajte ID domácej úlohy: ";
-            cin >> homeworkID;
-            cout << "Zadajte cestu k súboru: ";
-            cin.ignore();
-            getline(cin, filePath);
+    // Print the initial schedule
+    cout << "Initial Schedule:" << endl;
+    schedule.printSchedule();
+    cout << endl;
 
-            auto it = find_if(students.begin(), students.end(), [studentID](const Student& s) {
-                return s.getStudentID() == studentID;
-                });
+    // Check for conflicts between courses
+    if (schedule.checkConflikt(course1, course2)) {
+        cout << "There is a conflict between " << course1->getCourse_Name() << " and " << course2->getCourse_Name() << endl;
+    } else {
+        cout << "No conflict between " << course1->getCourse_Name() << " and " << course2->getCourse_Name() << endl;
+    }
 
-            if (it != students.end()) {
-                auto hw_it = find_if(homeworks.begin(), homeworks.end(), [homeworkID](const HomeWork& hw) {
-                    return hw.getHomeworkID() == homeworkID;
-                    });
+    if (schedule.checkConflikt(course1, course3)) {
+        cout << "There is a conflict between " << course1->getCourse_Name() << " and " << course3->getCourse_Name() << endl;
+    } else {
+        cout << "No conflict between " << course1->getCourse_Name() << " and " << course3->getCourse_Name() << endl;
+    }
 
-                if (hw_it != homeworks.end()) {
-                    it->submitHomework(homeworkID, filePath);
-                    submissions.push_back(Submission(submissions.size() + 1, homeworkID, studentID, filePath, "2024-07-20", "N/A"));
-                    saveSubmissions(submissions);
-                }
-                else {
-                    cout << "Domáca úloha nebola nájdená.\n";
-                }
-            }
-            else {
-                cout << "Študent nebol nájdený.\n";
-            }
-            break;
-        }
-        case 4:
-            cout << "Ukonèujem...\n";
-            break;
-        default:
-            cout << "Neplatná vo¾ba. Skúste znova.\n";
-            break;
-        }
-    } while (choice != 4);
+    // Remove a course from the schedule
+    schedule.remove_Course("Math");
+    cout << endl << "Schedule after removing Math course:" << endl;
+    schedule.printSchedule();
+
+    // Free dynamically allocated memory
+    delete course1;
+    delete course2;
+    delete course3;
 
     return 0;
 }
